1c1e2c2155c283115e73dbfe76fed2ebc826e78e
sunil-2000 <sunilsabnis10@gmail.com>
2023-04-19 10:30:11 -0400

    formatting and algo updates

--- algo.py
+++ algo.py
@@ -4,6 +4,7 @@ from grid import Map
 from functools import lru_cache
 from private import sk
 
+
 class Algo(Map):
     def __init__(self, y_lim, x_lim, start, n_obstacles=50, questions="question.txt"):
         super().__init__(y_lim, x_lim)
@@ -16,32 +17,34 @@ class Algo(Map):
         self.path_seq = []
 
         with open(questions) as file:
-          self.questions = [line.rstrip() for line in file]
+            self.questions = [line.rstrip() for line in file]
 
         self.marked_questions = [False for _ in range(len(self.questions))]
 
         # divide 2D space into k partitions where each partition is a 2D space
-        A = (y_lim * x_lim) // len(questions) # area of partition
+        A = (y_lim * x_lim) // len(questions)  # area of partition
         a_h, a_w = A // 9, 9
         self.q_map = [[-1 for _ in range(x_lim)] for _ in range(y_lim)]
-        
+
         i, j = 0, 0
         for k in range(len(questions)):
-          for y in range(i, i + a_h+1, 1): # i, j, upper-left expand partition each iter
-            for x in range(j, j + a_w+1, 1):
-              if y < y_lim and  x < x_lim:
-                self.q_map[y][x] = k
-          if j + a_w <= x_lim:
-             j += a_w  
-          else:
-             i += a_h
-             j = 0
+            for y in range(
+                i, i + a_h + 1, 1
+            ):  # i, j, upper-left expand partition each iter
+                for x in range(j, j + a_w + 1, 1):
+                    if y < y_lim and x < x_lim:
+                        self.q_map[y][x] = k
+            if j + a_w <= x_lim:
+                j += a_w
+            else:
+                i += a_h
+                j = 0
 
         for i in range(y_lim):
-           for j in range(x_lim):
-              if self.q_map[i][j] < 0:
-                 choice = np.random.randint(low=0, high=len(questions))
-                 self.q_map[i][j] = choice
+            for j in range(x_lim):
+                if self.q_map[i][j] < 0:
+                    choice = np.random.randint(low=0, high=len(questions))
+                    self.q_map[i][j] = choice
 
         # permute questions
         self.questions = np.random.permutation(self.questions)
@@ -51,56 +54,53 @@ class Algo(Map):
         """
         move constrained to following space: [1, y_lim-1]; [1, x_lim-1]
         """
+
         def obs_check(y, x):
-            if self.grid[y][x] < 0 or self.grid[y][x] == 1: # obstacle or boarder
+            if self.grid[y][x] < 0 or self.grid[y][x] == 1:  # obstacle or boarder
                 return False
-            return True 
+            return True
 
         stack = [(y, x)]
         while stack:
             i, j = stack.pop(-1)
-            self.path_seq.append((i,j))
+            self.path_seq.append((i, j))
 
             if self.grid[i][j] == 2:
-               return True 
+                return True
             self.grid[i][j] = -1
             # down
-            if i + 1 <= self.y_lim - 1 and obs_check(i+1, j): 
-              stack.append((i+1, j))
+            if i + 1 <= self.y_lim - 1 and obs_check(i + 1, j):
+                stack.append((i + 1, j))
             # up
-            if i - 1 >= 1 and obs_check(i-1, j):
-              stack.append((i-1, j))
+            if i - 1 >= 1 and obs_check(i - 1, j):
+                stack.append((i - 1, j))
             # left
-            if j - 1 >= 1 and obs_check(i, j-1):
-              stack.append((i, j-1))
+            if j - 1 >= 1 and obs_check(i, j - 1):
+                stack.append((i, j - 1))
             # right
-            if i + 1 <= self.x_lim -1 and obs_check(i, j+1):
-              stack.append((i, j+1))
+            if i + 1 <= self.x_lim - 1 and obs_check(i, j + 1):
+                stack.append((i, j + 1))
         return False
 
     def compute_path(self):
         self.reached = self._dfs(self.start[0], self.start[1])
-    
+
     def converse(self, i):
-      y, x = self.path_seq[i]
-      q_idx = self.q_map[y][x]
+        y, x = self.path_seq[i]
+        q_idx = self.q_map[y][x]
 
-      # plass call to GPT with input_q
-      input_q = self.questions[q_idx]
+        # plass call to GPT with input_q
+        input_q = self.questions[q_idx]
 
-      if self.marked_questions[q_idx]:
-         return input_q, False
+        if self.marked_questions[q_idx]:
+            return input_q, False
 
-      # completion = f"{' '.join(['hello' for _ in range(20)])}"
-      completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-           {"role": "user", "content": f"{input_q}"}
-         ],
-        max_tokens=50,
-        temperature=1.5,
-        stream=True
-       )
-      self.marked_questions[q_idx] = True
-      return input_q, completion 
-
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": f"{input_q}"}],
+            max_tokens=100,
+            temperature=1.6,
+            stream=True,
+        )
+        self.marked_questions[q_idx] = True
+        return input_q, completion
--- curse.py
+++ curse.py
@@ -1,124 +1,128 @@
-from curses import wrapper
 import curses
 import time
+from curses import wrapper
 from algo import Algo
 
+
 def game_init(win, algo, refresh_fn):
-  # boarder
-  boarder = algo.board_coords
-  for (y, x) in boarder:
-    win.addstr(y, x, "+", curses.color_pair(2))
+    # boarder
+    boarder = algo.board_coords
+    for y, x in boarder:
+        win.addstr(y, x, "+", curses.color_pair(2))
+        refresh_fn()
+
+    # obstacles
+    obstacles = algo.obstacles
+    for y, x in zip(obstacles[0], obstacles[1]):
+        win.addstr(y, x, "&", curses.color_pair(3))
+        refresh_fn()
+
+    # target coord
+    y_t, x_t = algo.target
+    win.addstr(y_t, x_t, "$", curses.color_pair(5))
     refresh_fn()
 
-  # obstacles
-  obstacles = algo.obstacles
-  for y, x in zip(obstacles[0], obstacles[1]):
-    win.addstr(y, x, "&", curses.color_pair(3))
-    refresh_fn()
-  
-  # target coord
-  y_t, x_t = algo.target
-  win.addstr(y_t, x_t, "$", curses.color_pair(5))
-  refresh_fn()
 
 def gpt_conversation(win, algo, refresh_fn, question, completion):
-  boarder_offset = 2
-  y_chat, x_chat = algo.y_lim + boarder_offset, 1
-  question = f"[userA]: {question}"
-  refresh_fn()
-  win.addstr(y_chat, x_chat, question)
+    boarder_offset = 2
+    y_chat, x_chat = algo.y_lim + boarder_offset, 1
+    question = f"[userA]: {question}"
+    refresh_fn()
+    win.addstr(y_chat, x_chat, question)
 
-  response = f"[userB]: "
-  win.addstr(y_chat + boarder_offset, x_chat, response)
-  refresh_fn()
+    response = f"[userB]: "
+    win.addstr(y_chat + boarder_offset, x_chat, response)
+    refresh_fn()
 
-  y_chat += boarder_offset
-  x_chat += len(response)
-  switch = 0
-  for chunk in completion:
-    if 'content' in chunk['choices'][0]['delta']: # grab token to write
-      s = chunk['choices'][0]['delta']['content']
+    y_chat += boarder_offset
+    x_chat += len(response)
 
-      win.addstr(y_chat, x_chat, s)
-      refresh_fn()
+    for chunk in completion:
+        if "content" in chunk["choices"][0]["delta"]:  # grab token to write
+            s = chunk["choices"][0]["delta"]["content"]
 
-      if x_chat + len(s) < algo.x_lim:
-        x_chat += len(s)
-      else:
-        y_chat += 1; x_chat = 1
+            win.addstr(y_chat, x_chat, s)
+            refresh_fn()
 
-      time.sleep(0.1)
-      switch += 1
+            if x_chat + len(s) < algo.x_lim:
+                x_chat += len(s)
+            else:
+                y_chat += 1
+                x_chat = 1
+
+        time.sleep(0.2)
+    time.sleep(3)
+
 
 def game_loop(win, algo, refresh_fn):
-  
-  obstacles = algo.obstacles
-  y_tar, x_tar = algo.target
+    obstacles = algo.obstacles
+    y_tar, x_tar = algo.target
 
-  i = 0
-  while i < len(algo.path_seq):
-    # refresh obstacle colors
-    color_obs = curses.color_pair(3)
-    if i % 2 == 0:
-      color_obs = curses.color_pair(4)
-    for y, x in zip(obstacles[0], obstacles[1]):
-      win.addstr(y, x, "&", color_obs)
-      refresh_fn()
+    i = 0
+    while i < len(algo.path_seq):
+        # refresh obstacle colors
+        color_obs = curses.color_pair(3)
+        if i % 2 == 0:
+            color_obs = curses.color_pair(4)
+        for y, x in zip(obstacles[0], obstacles[1]):
+            win.addstr(y, x, "&", color_obs)
+            refresh_fn()
 
-    # refresh target colors 
-    color_tar = curses.color_pair(5)
-    if i % 10 == 0:
-      color_tar = curses.color_pair(6)
-    win.addstr(y_tar, x_tar, "$", color_tar)
-    refresh_fn()
+        # refresh target colors
+        color_tar = curses.color_pair(5)
+        if i % 10 == 0:
+            color_tar = curses.color_pair(6)
+        win.addstr(y_tar, x_tar, "$", color_tar)
+        refresh_fn()
 
-    # update game positions
-    y, x = algo.path_seq[i]
-    win.addstr(y, x, ".", curses.color_pair(1))
-    refresh_fn()
-    
-    # talk to GPT, pause path-finding, output completion    
-    question, completion = algo.converse(i)
-    if completion:
-      # clear chat window
-      win.addstr(algo.y_lim + 2, 1, '')
-      refresh_fn()
-      win.clrtobot()
-      refresh_fn()
-      gpt_conversation(win, algo, refresh_fn, question, completion)
+        # update game positions
+        y, x = algo.path_seq[i]
+        win.addstr(y, x, ".", curses.color_pair(1))
+        refresh_fn()
 
-    i+=1
-    time.sleep(1)
+        # talk to GPT, pause path-finding, output completion
+        question, completion = algo.converse(i)
+        if completion:
+            # clear chat window
+            win.addstr(algo.y_lim + 2, 1, "")
+            refresh_fn()
+            win.clrtobot()
+            refresh_fn()
+            gpt_conversation(win, algo, refresh_fn, question, completion)
+
+        i += 1
+        time.sleep(0.2)
+
 
 def main(stdscr):
+    def refresh():
+        stdscr.refresh()
+        win.refresh()
 
-  def refresh():
-    stdscr.refresh()
-    win.refresh()
+    begin_x = 0
+    begin_y = 0
+    height = 500
+    width = 500
+    win = curses.newwin(height, width, begin_y, begin_x)
+    curses.noecho()
+    curses.cbreak()
+    stdscr.keypad(True)
 
-  begin_x = 0
-  begin_y = 0
-  height = 500
-  width = 500
-  win = curses.newwin(height, width, begin_y, begin_x)
-  curses.noecho()
-  curses.cbreak()
-  stdscr.keypad(True)
+    y_lim, x_lim = curses.LINES - 1, curses.COLS - 1
+    curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)  # player
+    curses.init_pair(2, curses.COLOR_MAGENTA, curses.COLOR_YELLOW)  # boarder
+    curses.init_pair(3, curses.COLOR_RED, curses.COLOR_WHITE)  # obstacle
+    curses.init_pair(4, curses.COLOR_WHITE, curses.COLOR_RED)  # obstacle
+    curses.init_pair(5, curses.COLOR_GREEN, curses.COLOR_BLACK)  # target
+    curses.init_pair(6, curses.COLOR_BLACK, curses.COLOR_GREEN)  # target
+    curses.init_pair(7, curses.COLOR_BLACK, curses.COLOR_YELLOW)  # player
 
-  y_lim, x_lim = curses.LINES - 1, curses.COLS - 1
-  curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK) # player
-  curses.init_pair(2, curses.COLOR_MAGENTA, curses.COLOR_YELLOW) # boarder
-  curses.init_pair(3, curses.COLOR_RED, curses.COLOR_WHITE) # obstacle
-  curses.init_pair(4, curses.COLOR_WHITE, curses.COLOR_RED) # obstacle
-  curses.init_pair(5, curses.COLOR_GREEN, curses.COLOR_BLACK) # target 
-  curses.init_pair(6, curses.COLOR_BLACK, curses.COLOR_GREEN) # target 
-  curses.init_pair(7, curses.COLOR_BLACK, curses.COLOR_YELLOW) # player
+    algo = Algo(y_lim // 2, x_lim, start=(y_lim // 4, x_lim // 2))
+    algo.compute_path()
+    game_init(win, algo, refresh)
+    game_loop(win, algo, refresh)
 
-  algo = Algo(y_lim//2, x_lim, start=(y_lim//4,x_lim//2))
-  algo.compute_path()
-  game_init(win, algo, refresh)
-  game_loop(win, algo, refresh)
+    stdscr.getch()
 
-  stdscr.getch()
 
 wrapper(main)
